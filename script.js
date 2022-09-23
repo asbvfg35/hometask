c27355ebde2a6d64a6bff4188d3fca81b77da69b
Andrey <asbvfg35@gmail.com>
2022-09-23 17:22:45 +0300

    37

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 "use strict";
 
-let numberOfFilms;
-function start() {
-  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-  }
-}
-
-start();
-
 const personalMoovieDB = {
-  count: numberOfFilms,
+  count: 0,
   moovies: {},
   actors: {},
   genres: [],
   privat: false,
 };
+
+personalMoovieDB.start = start;
+personalMoovieDB.rememberMyFilms = rememberMyFilms;
+personalMoovieDB.detectPersonalLevel = detectPersonalLevel;
+personalMoovieDB.detectPersonalLevel = detectPersonalLevel;
+personalMoovieDB.showMyDB = showMyDB;
+personalMoovieDB.writeYourGenres = writeYourGenres;
+
+function start() {
+  personalMoovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  while (personalMoovieDB.count == "" || personalMoovieDB.count == null || isNaN(numberOfFilms)) {
+    personalMoovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
 
 function rememberMyFilms() {
   for (let i = 0; i < 2; i++) {
@@ -24,17 +28,12 @@ function rememberMyFilms() {
     const b = prompt('На сколько оцените его', '');
 
     if (a != null && b != null && a.length < 50 && a != '' && b != '') {
-
       personalMoovieDB.moovies[a] = b;
-
     } else {
-
       i--;
     }
   }
 }
-
-rememberMyFilms();
 
 function detectPersonalLevel() {
   if (personalMoovieDB.count < 10) {
@@ -46,15 +45,28 @@ function detectPersonalLevel() {
   } else console.log("Произошла ошибка");
 }
 
-detectPersonalLevel();
-
 function showMyDB(hiden) {
   if (!hiden) console.log(personalMoovieDB);
 }
 
 function writeYourGenres() {
   for (let i = 1; i <= 3; i++) {
-    personalMoovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`)
+    personalMoovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    let prom = personalMoovieDB.genres[i - 1];
+    if (prom !== null || prom !== '') {
+      personalMoovieDB.genres[i - 1] = prom;
+    } else {
+      i--;
+    }
   }
+  personalMoovieDB.genres.forEach((item, i) => console.log(`Любимый жанр ${i + 1} - это ${item}`));
 }
+
+personalMoovieDB.toggleVisibleMyDB = function () {
+  if (personalMoovieDB.privat) {
+    personalMoovieDB.privat = false;
+  } else {
+    personalMoovieDB.privat = true;
+  }
+};
 
